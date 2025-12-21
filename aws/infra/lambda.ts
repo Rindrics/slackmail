21313181d82764b37213a0bfc3c99c66b63917ad
Akira Hayashi <rindrics@gmail.com>
2025-12-21 17:06:06 +0900

    Define Lambda function

---
+++ aws/infra/lambda.ts
@@ -0,0 +1,79 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { stackName, tags } from "./config";
+import { emailBucket } from "./s3";
+
+// IAM role for Lambda execution
+export const lambdaRole = new aws.iam.Role("lambda-role", {
+  assumeRolePolicy: JSON.stringify({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "sts:AssumeRole",
+        Principal: {
+          Service: "lambda.amazonaws.com",
+        },
+        Effect: "Allow",
+      },
+    ],
+  }),
+  tags,
+});
+
+// Policy: CloudWatch Logs access
+export const lambdaLogsPolicy = new aws.iam.RolePolicy("lambda-logs-policy", {
+  role: lambdaRole.id,
+  policy: JSON.stringify({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        Resource: "arn:aws:logs:*:*:*",
+      },
+    ],
+  }),
+});
+
+// Policy: S3 read access for email bucket
+export const lambdaS3Policy = new aws.iam.RolePolicy("lambda-s3-policy", {
+  role: lambdaRole.id,
+  policy: pulumi.interpolate`{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Resource": "${emailBucket.arn}/*"
+      }
+    ]
+  }`,
+});
+
+// Lambda function
+export const boltLambda = new aws.lambda.Function("bolt-lambda", {
+  runtime: aws.lambda.Runtime.NodeJS20dX,
+  handler: "index.handler",
+  role: lambdaRole.arn,
+  code: new pulumi.asset.AssetArchive({
+    // Placeholder: will be replaced by esbuild output
+    "index.js": new pulumi.asset.StringAsset(
+      'exports.handler = async () => ({ statusCode: 200, body: "placeholder" });',
+    ),
+  }),
+  timeout: 30,
+  memorySize: 256,
+  environment: {
+    variables: {
+      NODE_ENV: stackName,
+      EMAIL_BUCKET_NAME: emailBucket.bucket,
+    },
+  },
+  tags,
+});
